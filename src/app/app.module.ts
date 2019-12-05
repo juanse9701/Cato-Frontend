87e7e98ae1a7d0f920df16417d88644f969b5f46
Diego Aguilera <diego.aguilera@unillanos.edu.co>
2019-12-05 12:16:27 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './core/modules/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
