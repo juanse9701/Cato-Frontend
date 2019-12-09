62095e695fab3271f94c710026d632bf6113800b
Diego Alejandro Aguilera Martinez <diego.aguilera@unillanos.edu.co>
2019-12-09 06:56:40 -0500

    Lineas tecnologicas, casos de exito

--- src/app/core/modules/home/home.component.ts
+++ src/app/core/modules/home/home.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  clientes: any[] = [
+    {
+      nombre: "sector privador",
+      clientes: [
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        }
+      ]
+    },
+    {
+      nombre: "sector público",
+      clientes: [
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        }
+      ]
+    },
+    {
+      nombre: "ong's",
+      clientes: [
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        },
+        {
+          img: "assets/img/realityapp.png"
+        }
+      ]
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
