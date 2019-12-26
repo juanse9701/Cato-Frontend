c30945a7f854845272c0c67b69653301eb6dcf15
Diego Aguilera <diego.aguilera@unillanos.edu.co>
2019-12-26 07:28:49 -0500

    Menu fijo on scroll

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
@@ -10,6 +10,17 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 })
 export class NavbarComponent implements OnInit {
   tipo: string;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+    if (window.pageYOffset > 1) {
+      let element = document.getElementById('header');
+      element.classList.add('sticky');
+    } else {
+      let element = document.getElementById('header');
+      element.classList.remove('sticky');
+    }
+  }
   constructor(private router: Router, private location: Location) {
     this.router.events.subscribe(event => {
       if (this.location.path() === '') {
