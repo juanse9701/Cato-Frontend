d63c5146ca4e551137895c1c555a89fabc0df11c
Sebastian peralta <juan.peralta@unillanos.edu.co>
2020-07-29 12:49:59 -0500

    docs: services documentados

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -5,6 +5,9 @@ import { Contact } from '../core/interface/contact.interface';
 import { map } from 'rxjs/operators';
 import { QUERYCONTACT } from '../core/graphql/Query';
 
+/**
+ * Servicio encargado de proveer información acerca de la pagina de contacto.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +15,14 @@ export class ContactService {
 
   constructor(private apollo: Apollo) { }
 
+  /**
+   * @method getContactPage
+   * @param lang parametro de tipo estring que recibe el idioma en el cual se solicitara la información
+   *
+   * Método encargado de solicitar la información de la página de contacto
+   *
+   * @return devuelve un nuevo observable con la información de la pagina de contacto
+   */
   getContactPage(lang: string = 'ES'): Observable<Contact> {
     return this.apollo.query<Contact>({
       query: QUERYCONTACT,
--- src/app/services/pages.service.ts
+++ src/app/services/pages.service.ts
@@ -5,6 +5,9 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Page } from '../core/interface/page.interface';
 
+/**
+ * Servicio encargado de proveer información de las páginas que le sean solicitadas por los distintos componentes.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +15,15 @@ export class PagesService {
 
   constructor(private apollo: Apollo) {}
 
+  /**
+   * @method getPage
+   * @param slug paramero de tipo string que sirve como identificador de la página
+   * @param lang parametro de tipo estring que recibe el idioma en el cual se solicitara la información
+   *
+   * Método encargado de solicitar la información de una página en un idioma.
+   *
+   * @return Un nuevo observable con la información de la página en el idioma solicitado.
+   */
   getPage(slug: string, lang: string = 'ES'): Observable<Page> {
     return this.apollo.query({
       query: QUERYGENERAL,
